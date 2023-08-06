4e184f7fa87c00805ef8772afd062cc2e67dd6ba
escobardie <diego_escobar@live.com>
2023-08-06 05:12:13 -0300

    CK EDITOR
    SE CREAR CKEDITOR

--- letter/forms.py
+++ letter/forms.py
@@ -12,3 +12,8 @@ class MailMessageForm(forms.ModelForm):
     class Meta:
         model = MailMessage
         fields = '__all__'
+
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'message': forms.Textarea(attrs={'class': 'form-control'}),
+        }
